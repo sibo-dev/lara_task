8de4e2cda77c760c9664d2e1ae8e7a5c5f665c60
Sibonelo Nkosi <sibonelo.dev@gmail.com>
2024-04-06 11:22:15 +0200

    Updated the dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,31 +2,32 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
+            <div class="float-right">
+            <a href="{{ route('tasks') }}">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded">
+                {{ __("View Your Tasks") }}
+                </button>
+            </a>
+            </div>
         </h2>
     </x-slot>
 
-    <div class="py-6">
-        <div class="max-w-5x1 mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col items-center justify-center bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in as") }} {{ Auth::user()->name }}
-                </div>
-                <div>
-                <a href="{{ route('tasks') }}">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded">
-                        {{ __("View Your Tasks") }}
-                    </button>
-                </a>
-                </div>
-            </div>
-        </div>
-    </div>
+    
+
     <div class="py-12">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col items-center justify-center bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Message From the author: Use the following space to display any additional information or links to other parts of your application.
-                    Perhaps task history or most commonly completed or repeated tasks and a graph.
+            <div class="grid grid-cols-2 gap-4">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6">
+                        <h3 class="text-lg font-semibold mb-4">Open Tasks</h3>
+                        <p>Display a list of open tasks here.</p>
+                    </div>
+                </div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6">
+                        <h3 class="text-lg font-semibold mb-4">Closed Tasks</h3>
+                        <p>Display a list of closed tasks here.</p>
+                    </div>
                 </div>
             </div>
         </div>
